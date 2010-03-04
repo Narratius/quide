b9ded7df79acbecc492e3b674f26d20059851d7b
Narratius <Narratius@2d237ac8-0cd4-11df-8457-13bc90d6e451>
2010-03-04 12:38:46 +0000

    Сквозное редактирование действий

--- IDE/dobTextActionEdit.pas
+++ IDE/dobTextActionEdit.pas
@@ -9,6 +9,7 @@ uses
 type
   TdobTextFrame = class(TFrame)
     TextMemo: TMemo;
+    procedure TextMemoChange(Sender: TObject);
   private
     { Private declarations }
   public
@@ -18,5 +19,21 @@ type
 implementation
 
 {$R *.dfm}
+
+procedure TdobTextFrame.TextMemoChange(Sender: TObject);
+var
+ l_TextHeight : Integer;
+ l_Canvas: TCanvas;
+ l_P: TWinControl;
+begin
+ l_P:= Parent;
+ if l_P <> nil then
+ begin
+  while not (l_P is TCustomForm) do l_P:= l_P.Parent;
+  l_Canvas:= (l_P as TCustomForm).Canvas;
+  l_TextHeight:= l_Canvas.TextHeight('A')*(TextMemo.Lines.Count+1);
+  ClientHeight:= l_textHeight;
+ end; 
+end;
 
 end.
--- IDE/guiLocEditDlgEx.pas
+++ IDE/guiLocEditDlgEx.pas
@@ -9,8 +9,6 @@ uses
 type
   TLocationEditExDlg = class(TForm)
     EditPlace: TScrollBox;
-    OKButton: TButton;
-    CancelButton: TButton;
     ActionList1: TActionList;
     PopupMenu1: TPopupMenu;
     TextAction: TAction;
@@ -24,11 +22,24 @@ type
     N4: TMenuItem;
     N5: TMenuItem;
     N6: TMenuItem;
+    TuneAction: TAction;
+    N7: TMenuItem;
+    Panel1: TPanel;
+    OKButton: TButton;
+    CancelButton: TButton;
+    Label1: TLabel;
+    editCaption: TEdit;
+    procedure FormDestroy(Sender: TObject);
     procedure AddTextExecute(Sender: TObject);
+    procedure EditPlaceResize(Sender: TObject);
+    procedure VarActionExecute(Sender: TObject);
   private
+    f_EnableResize: Boolean;
     f_Location: TdcLocation;
+    procedure ControlResize(Sender: TObject);
     procedure CreateAction(anAction: TdcAction);
     procedure CreateButton(anButton: TdcButtonAction);
+    procedure GetLocationData;
     function pm_GetLocation: TdcLocation;
     procedure pm_SetLocation(const Value: TdcLocation);
     procedure RefreshList;
@@ -56,9 +67,16 @@ begin
  try
   Location:= aLocation;
   Result:= IsPositiveResult(ShowModal);
+  if Result then
+   aLocation.Assign(Location)
  finally
   Free;
  end;
+end;
+
+procedure TLocationEditExDlg.FormDestroy(Sender: TObject);
+begin
+ f_Location.Free;
 end;
 
 procedure TLocationEditExDlg.AddTextExecute(Sender: TObject);
@@ -66,9 +84,35 @@ var
  l_A: TdcTextAction;
 begin
  // Добавить фрем для редактирования текста
- l_A:= TdcTextAction.Create(Location.Model);
- Location.AddAction(l_A);
+ l_A:= TdcTextAction.Create(f_Location.Model);
+ f_Location.AddAction(l_A);
  CreateAction(l_A);
+end;
+
+procedure TLocationEditExDlg.ControlResize(Sender: TObject);
+var
+ i: Integer;
+ l_Move: Boolean;
+ l_Delta: Integer;
+begin
+ // Один из контролов изменился. Нужно сдвинуть вниз всех, стоящих за ним
+ if f_EnableResize then
+ begin
+  l_Move:= False;
+  l_Delta:= 0;
+  for i:= 0 to EditPlace.ControlCount-1 do
+  begin
+   if l_Move then
+    EditPlace.Controls[i].Top:= EditPlace.Controls[i].Top + l_Delta
+   else
+   if EditPlace.Controls[i] = Sender then
+   begin
+    l_Move:= True;
+    if i < Pred(EditPlace.ControlCount) then
+     l_Delta:= EditPlace.Controls[i].Top + EditPlace.Controls[i].Height - EditPlace.Controls[i+1].Top;
+   end;
+  end;
+ end; 
 end;
 
 procedure TLocationEditExDlg.CreateAction(anAction: TdcAction);
@@ -96,7 +140,7 @@ begin
      TVarActionFrame(l_Frame).Variable:= TdcVariableAction(anAction).Variable;
      TVarActionFrame(l_Frame).Value:= TdcVariableAction(anAction).Value;
     end; // TdcVariableAction(anAction).Variable <> nil
-    TVarActionFrame(l_Frame).Script:= Location.Model;
+    TVarActionFrame(l_Frame).Script:= f_Location.Model;
    end;
  end; // case
 
@@ -107,6 +151,7 @@ begin
   l_Frame.Top:= 1;
  l_Frame.Width:= EditPlace.ClientWidth;
  l_Frame.Parent:= EditPlace;
+ l_Frame.OnResize:= ControlResize;
 end;
 
 procedure TLocationEditExDlg.CreateButton(anButton: TdcButtonAction);
@@ -114,10 +159,46 @@ begin
   // TODO -cMM: TLocationEditExDlg.CreateButton default body inserted
 end;
 
+procedure TLocationEditExDlg.EditPlaceResize(Sender: TObject);
+var
+ i: Integer;
+begin
+ // Изменился размер - нужно поменять ширины контролам
+ for i:= 0 to EditPlace.ControlCount-1 do
+  EditPlace.Controls[i].Width:= EditPlace.ClientWidth;
+end;
+
+procedure TLocationEditExDlg.GetLocationData;
+var
+ i: Integer;
+ l_A: TdcAction;
+ l_LocName: string;
+begin
+ f_Location.Caption:= editCaption.Text;
+ for i:= 0 to f_Location.ActionsCount-1 do
+ begin
+  l_A:= f_Location.Actions[i];
+  case l_A.ActionType of
+   atText: TdcTextAction(l_A).Description:= (EditPlace.Controls[i] as TdobTextFrame).TextMemo.Lines;
+   atGoto:
+    begin
+     l_LocName:= TGotoActionFrame(EditPlace.Controls[i]).GotoLocation;
+     TdcGotoAction(l_A).Location:= f_Location.Model.FindLocation(l_LocName);
+    end;
+   atVariable:
+    with TdcVariableAction(l_A) do
+    begin
+     Variable:= TVarActionFrame(EditPlace.Controls[i]).Variable;
+     Value:= TVarActionFrame(EditPlace.Controls[i]).Value;
+    end;
+  end;
+ end;
+end;
+
 function TLocationEditExDlg.pm_GetLocation: TdcLocation;
 begin
-  // TODO перебрать элементы и обновить значения действий
-  Result := f_Location;
+ GetLocationData;
+ Result := f_Location;
 end;
 
 procedure TLocationEditExDlg.pm_SetLocation(const Value: TdcLocation);
@@ -132,21 +213,31 @@ var
   I: Integer;
   l_C: TControl;
 begin
-
+ f_EnableResize:= False;
  while EditPlace.ControlCount > 0 do
  begin
   l_C:= EditPlace.Controls[0];
   EditPlace.RemoveControl(l_C);
   FreeAndNil(l_C);
  end; // while EditPlace.ControlCount > 0
- for I := 0 to Location.ActionsCount - 1 do
+ for I := 0 to f_Location.ActionsCount - 1 do
  begin
-  if Location.Actions[i].ActionType = atButton then
-   CreateButton(Location.Actions[i] as TdcButtonAction)
+  if f_Location.Actions[i].ActionType = atButton then
+   CreateButton(f_Location.Actions[i] as TdcButtonAction)
   else
-   CreateAction(Location.Actions[i]);
+   CreateAction(f_Location.Actions[i]);
  end;
+ editCaption.Text:= f_Location.Caption;
+ f_EnableResize:= True;
+end;
 
+procedure TLocationEditExDlg.VarActionExecute(Sender: TObject);
+var
+ l_A: TdcAction;
+begin
+ l_A:= TdcVariableAction.Create(f_Location.Model);
+ f_Location.AddAction(l_A);
+ CreateAction(l_A);
 end;
 
 end.
