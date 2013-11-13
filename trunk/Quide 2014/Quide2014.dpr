3b442119a82e6b7437d58d4e3daff17c9f7bbc71
Narratius@gmail.com <Narratius@gmail.com@2d237ac8-0cd4-11df-8457-13bc90d6e451>
2013-11-13 20:09:51 +0000

    Переход на SimpleGraph.

---
+++ trunk/Quide 2014/Quide2014.dpr
@@ -0,0 +1,23 @@
+program Quide2014;
+
+uses
+  Forms,
+  Main in 'Main.pas' {MainForm},
+  DesignProp in 'DesignProp.pas' {DesignerProperties},
+  ObjectProp in 'ObjectProp.pas' {ObjectProperties},
+  LinkProp in 'LinkProp.pas' {LinkProperties},
+  NodeProp in 'NodeProp.pas' {NodeProperties},
+  AboutDelphiArea in 'AboutDelphiArea.pas' {About},
+  UsageHelp in 'UsageHelp.pas' {HelpOnActions},
+  MarginsProp in 'MarginsProp.pas' {MarginDialog},
+  AlignDlg in 'AlignDlg.pas' {AlignDialog},
+  SizeDlg in 'SizeDlg.pas' {SizeDialog};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.Title := 'Quest IDE 2014';
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
