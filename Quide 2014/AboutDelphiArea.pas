35b45f0c2333124718f62e3f6933666d47641d0c
Narratius@gmail.com <Narratius@gmail.com@2d237ac8-0cd4-11df-8457-13bc90d6e451>
2013-11-13 20:09:51 +0000

    Переход на SimpleGraph.

---
+++ Quide 2014/AboutDelphiArea.pas
@@ -0,0 +1,26 @@
+unit AboutDelphiArea;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, StdCtrls, Vcl.Imaging.pngimage;
+
+type
+  TAbout = class(TForm)
+    btnOk: TButton;
+    Image1: TImage;
+    procedure FormCreate(Sender: TObject);
+  end;
+
+
+implementation
+
+{$R *.dfm}
+
+procedure TAbout.FormCreate(Sender: TObject);
+begin
+  SetBounds(Screen.Width - Width - 30, 50, Width, Height);
+end;
+
+end.
