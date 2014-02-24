596bbc293a006d42d7ef147855c9947554a9bf6e
Narratius@gmail.com <Narratius@gmail.com@2d237ac8-0cd4-11df-8457-13bc90d6e451>
2014-02-24 13:52:30 +0000

    Сохранение-загрузка свойств

--- internal/Properties/PropertyIntf.pas
+++ internal/Properties/PropertyIntf.pas
@@ -3,20 +3,20 @@ unit PropertyIntf;
 interface
 
 uses
-  SysUtils, Windows, Messages, Classes, Graphics, Controls, Forms, Dialogs;
+  SysUtils, Windows, Messages, Classes, Xml.XMLIntf;
 
 type
   //1 Обеспечивает сохранение-загрузку
-  IquideStore = interface(IInterface)
+  IPropertyStore = interface
     ['{1604F322-5F86-4359-A44A-33EE3CB1F6C7}']
-    procedure Load(aStream: TStream); stdcall;
-    procedure Save(aStream: TStream); stdcall;
+    procedure LoadFromXML(Element: IXMLNode);
+    procedure SaveToXML(Element: IXMLNode);
   end;
 
-  IPropertyControlValue = interface(IInterface)
+  IPropertyControlValue = interface
     ['{7660D972-81B7-4062-AF86-4517B58AA55E}']
-    function pm_GetValue: Variant; stdcall;
-    procedure pm_SetValue(const Value: Variant); stdcall;
+    function pm_GetValue: Variant;
+    procedure pm_SetValue(const Value: Variant);
     property Value: Variant read pm_GetValue write pm_SetValue;
   end;
 
--- internal/Properties/Propertys.pas
+++ internal/Properties/Propertys.pas
@@ -3,7 +3,7 @@ unit Propertys;
 interface
 
 uses
-  SysUtils, Windows, Messages, Classes, Graphics, Controls, Forms, Dialogs,
+  SysUtils, Windows, Messages, Classes, Xml.XMLIntf, Contnrs,
   PropertyIntf;
 
 type
@@ -16,10 +16,10 @@ type
                   ptProperties // TScrollBox (Вложенные свойства)
                   );
 
-  TProperty = class(TCollectionItem)
+  TProperty = class(TPersistent)
   private
     f_Alias: string;
-    f_Caption: TCaption;
+    f_Caption: String;
     f_Event: TNotifyEvent;
     f_PropertyType: TPropertyType;
     f_Value: Variant;
@@ -27,7 +27,7 @@ type
   public
     procedure Assign(Source: TPersistent); override;
     property Alias: string read f_Alias write f_Alias;
-    property Caption: TCaption read f_Caption write f_Caption;
+    property Caption: String read f_Caption write f_Caption;
     property PropertyType: TPropertyType read f_PropertyType write
         f_PropertyType;
     property Value: Variant read f_Value write f_Value;
@@ -37,54 +37,38 @@ type
 
 
   TPropertyFunc = function (aItem: TProperty): Boolean of object;
-  TProperties = class(TCollection, IquideStore)
+  TProperties = class(TInterfacedObject, IpropertyStore)
   private
     f_Changed: Boolean;
+    f_Items: TObjectList;
     function FindProperty(aAlias: String): TProperty;
     function pm_GetAliasItems(Alias: String): TProperty;
     function pm_GetItems(Index: Integer): TProperty;
     function pm_GetValues(Alias: String): string;
     procedure pm_SetAliasItems(Alias: String; aValue: TProperty);
     procedure pm_SetChanged(const Value: Boolean);
-    procedure pm_SetItems(Index: Integer; aValue: TProperty);
     procedure pm_SetValues(Alias: String; const Value: string);
+    function pm_GetCount: Integer;
   public
     constructor Create;
     function Add: TProperty;
-    procedure Assign(Source: TPersistent); override;
+    procedure Assign(Source: TProperties);
     procedure Define(const aAlias, aCaption: String; aType: TPropertyType;
         aVisible: Boolean = True; aEvent: TNotifyEvent = nil);
     procedure IterateAll(aFunc: TPropertyFunc);
-    procedure Load(aStream: TStream);
-    procedure Save(aStream: TStream);
+    procedure LoadFromXML(Element: IXMLNode);
+    procedure SaveToXML(Element: IXMLNode);
+    procedure TestProc;
     property AliasItems[Alias: String]: TProperty read pm_GetAliasItems write
         pm_SetAliasItems; default;
     property Changed: Boolean read f_Changed write pm_SetChanged;
-    property Items[Index: Integer]: TProperty read pm_GetItems write
-        pm_SetItems;
+    property Count: Integer read pm_GetCount;
+    property Items[Index: Integer]: TProperty read pm_GetItems;
     property Values[Alias: String]: string read pm_GetValues write pm_SetValues;
   end;
 
-function Property2Control(aProperty: TPropertyType): TControlClass;
-
 
 implementation
-
-uses
-  fmx.Edit;
-
-function Property2Control(aProperty: TPropertyType): TControlClass;
-begin
-  case aProperty of
-   ptString: ;// TEdit;
-   ptInteger:;   // TEdit
-   ptText:;      // TMemo
-   ptBoolean:;   // TRadioGroup (TCombobox)
-   ptChoice:;    // TComboBox
-   ptAction:;    // TButton
-   ptProperties:; // TScrollBox (Вложенные свойства)
-  end;
-end;
 
 {
 ********************************** TProperty ***********************************
@@ -107,34 +91,23 @@ end;
 }
 constructor TProperties.Create;
 begin
- inherited Create(TProperty);
+ F_Items := TObjectList.Create(True);
 end;
 
 function TProperties.Add: TProperty;
 begin
- Result := TProperty(inherited Add);
+ Result := TProperty.Create;
+ f_Items.Add(Result);
 end;
 
-procedure TProperties.Assign(Source: TPersistent);
+procedure TProperties.Assign(Source: TProperties);
 var
   I: Integer;
 begin
-  if Source is TProperties then
-  begin
-    BeginUpdate;
-    try
-      // Replaces call to Clear to avoid BeginUpdate/try/finally/EndUpdate block
-      while Count > 0 do
-        Items[Count - 1].DisposeOf;
+  f_Items.Clear; //
 
-      for I := 0 to TProperties(Source).Count - 1 do
-        Add.Assign(TProperties(Source).Items[I]);
-    finally
-      EndUpdate;
-    end;
-    Exit;
-  end;
-  inherited Assign(Source);
+  for I := 0 to TProperties(Source).Count - 1 do
+   Add.Assign(TProperties(Source).Items[I]);
 end;
 
 procedure TProperties.Define(const aAlias, aCaption: String; aType:
@@ -142,6 +115,7 @@ procedure TProperties.Define(const aAlias, aCaption: String; aType:
 var
  l_P: TProperty;
 begin
+ // Проверить валидность Alias
  l_P:= Add;
  l_P.Alias:= aAlias;
  l_P.Caption:= aCaption;
@@ -156,9 +130,9 @@ var
 begin
    Result:= nil;
    for i:= 0 to Pred(Count) do
-    if AnsiSameText(TProperty(inherited Items[i]).Alias, aAlias) then
+    if AnsiSameText(Items[i].Alias, aAlias) then
     begin
-     Result:= TProperty(inherited Items[i]);
+     Result:= Items[i];
      break
     end;
 end;
@@ -177,9 +151,11 @@ begin
   end;
 end;
 
-procedure TProperties.Load(aStream: TStream);
+procedure TProperties.LoadFromXML(Element: IXMLNode);
 var
-  i: Integer;
+  i, j: Integer;
+  l_E: IXMLNode;
+  l_Strings: TStrings;
 begin
   for i:= 0 to Pred(Count) do
   begin
@@ -197,7 +173,7 @@ begin
            l_Strings.Text:= Value;
            l_E.SetAttribute('TextCount', l_Strings.Count);
            for j:= 0 to Pred(l_Strings.Count) do
-            l_E.AddChild('Text'):= l_Strings[j];
+            l_E.AddChild('Text').Text:= l_Strings[j];
           finally
            FreeAndNil(l_Strings);
           end;
@@ -205,23 +181,29 @@ begin
          l_E:= nil;
         end;
        end;
-      ptBoolean: : Value:= Element.GetAttribute(Alias);   // TRadioGroup (TCombobox)
+      ptBoolean: Value:= Element.GetAttribute(Alias);   // TRadioGroup (TCombobox)
       ptChoice,    // TComboBox
       ptAction,    // TButton
-      ptProperties // TScrollBox (Вложенные свойства)
+      ptProperties: ; // TScrollBox (Вложенные свойства)
      end; // case
    end; // with Items[i]
   end; // for i
 end;
+
 
 function TProperties.pm_GetAliasItems(Alias: String): TProperty;
 begin
   Result:= FindProperty(Alias);
 end;
 
+function TProperties.pm_GetCount: Integer;
+begin
+ Result:= f_Items.Count;
+end;
+
 function TProperties.pm_GetItems(Index: Integer): TProperty;
 begin
-  Result:= TProperty(Inherited GetItem(Index));
+  Result:= TProperty(f_Items[Index]);
 end;
 
 function TProperties.pm_GetValues(Alias: String): string;
@@ -244,11 +226,6 @@ begin
  f_Changed := Value;
 end;
 
-procedure TProperties.pm_SetItems(Index: Integer; aValue: TProperty);
-begin
- inherited Items[Index]:= aValue;
-end;
-
 procedure TProperties.pm_SetValues(Alias: String; const Value: string);
 begin
  // TODO -cMM: TProperties.pm_SetValues необходимо написать реализацию
@@ -256,9 +233,11 @@ begin
  Changed:= True;
 end;
 
-procedure TProperties.Save(aStream: TStream);
+procedure TProperties.SaveToXML(Element: IXMLNode);
 var
-  i: Integer;
+  i, j: Integer;
+  l_E: IXMLNode;
+  l_Strings: TStrings;
 begin
   for i:= 0 to Pred(Count) do
   begin
@@ -276,7 +255,7 @@ begin
            l_Strings.Text:= Value;
            l_E.SetAttribute('TextCount', l_Strings.Count);
            for j:= 0 to Pred(l_Strings.Count) do
-            l_E.AddChild('Text'):= l_Strings[j];
+            l_E.AddChild('Text').Text:= l_Strings[j];
           finally
            FreeAndNil(l_Strings);
           end;
@@ -284,15 +263,18 @@ begin
          l_E:= nil;
         end;
        end;
-      ptBoolean: : Element.SetAttribute(Alias, Value);   // TRadioGroup (TCombobox)
+      ptBoolean: Element.SetAttribute(Alias, Value);   // TRadioGroup (TCombobox)
       ptChoice,    // TComboBox
       ptAction,    // TButton
-      ptProperties // TScrollBox (Вложенные свойства)
+      ptProperties: ; // TScrollBox (Вложенные свойства)
      end; // case
    end; // with Items[i]
   end; // for i
 end;
 
-
+procedure TProperties.TestProc;
+begin
+ //
+end;
 
 end.
